0998c310da46a33a240a252d8899e8f6fc32ccbe
Fabian <156524829+fabianbola@users.noreply.github.com>
2024-11-27 21:31:15 -0500

    Front-end commit

---
+++ Assignment 5/DanielBolanos_COMP228Lab5/src/main/java/com/example/danielbolanos_comp228lab5/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.danielbolanos_comp228lab5;
+
+
+import javafx.scene.control.Alert;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.application.Platform;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+import java.lang.reflect.Array;
+
+public class HelloController {
+
+    @FXML
+    private TextField TextPlayerID, TextFirstName, TextLastName, TextAddress, TextPostalCode, TextProvidence, TextPhoneNumber, TextGameID, TextGameTitle, TextScore;
+
+
+}
